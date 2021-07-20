03196689b1d1bca95336b902d2b2a6364664e866
Shreyansh Anchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2021-07-20 22:23:46 +0530

    Sleep Logs

---
+++ Assets/Scripts/Input/SleepLog.cs
@@ -0,0 +1,39 @@
+using System;
+// ReSharper disable once RedundantUsingDirective
+using FantomLib;
+using System.Globalization;
+using UnityEngine;
+
+public class SleepLog : MonoBehaviour
+{
+    private Log log;
+
+    [SerializeField] private StringHolder startTime, endTime;
+    
+    public void Submit()
+    {
+        try
+        { 
+            //log base
+            log.logType = Log.LogType.Sleep;
+            log.timeOfLog = DateTime.Now;
+            log.startTime = DateTime.ParseExact(startTime.GetString(), "HH:mm", CultureInfo.InvariantCulture);
+            //custom log
+            log.endTime = DateTime.ParseExact(endTime.GetString(), "HH:mm", CultureInfo.InvariantCulture);
+
+#if UNITY_EDITOR
+            Debug.Log("Sleep Submitted");
+#else
+            AndroidPlugin.ShowToast($"Sleep Submitted\n{log.startTime}");
+#endif
+        }
+        catch (Exception e)
+        {
+#if UNITY_EDITOR
+            Debug.Log(e.Message);
+#else
+            AndroidPlugin.ShowToast(e.Message);
+#endif
+        }
+    }
+}
