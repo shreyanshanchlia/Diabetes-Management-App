4b859339269a6ae7083bd7d1165dd4a48485ece4
Shreyansh Anchlia <48558967+shreyanshanchlia@users.noreply.github.com>
2021-12-03 06:59:36 +0530

    Daily Streaks added

--- Assets/Scripts/Achievements/Achievement.cs
+++ Assets/Scripts/Achievements/Achievement.cs
@@ -1,10 +1,20 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Achievement", menuName = "Achievements/New")]
 public class Achievement : ScriptableObject
 {
-    public string achievementID;
+    public string achievementID = "";
     public Sprite achievementLogo;
     public string achievementName;
     public string achievementDescription;
+    [HideInInspector] public DateTime timeOfAchievement;
+}
+
+public class DailyStreak
+{
+    public DateTime startDate = DateTime.Today;
+    public DateTime lastUpdated = DateTime.Today.AddDays(-1);
+    public uint streakLength = 0;
+    public uint longestStreak = 0;
 }
